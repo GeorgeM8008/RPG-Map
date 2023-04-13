12f8d68c64781401e9793928415878398b6874e7
GeorgeM8008 <>
2023-04-13 08:01:32 +0000

    Added item nested dictionary and finished comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-                                   #George Mi, CS30, March 
+#George Mi, CS30, March 
 #This is a simple map for an RPG game
 
 
@@ -6,53 +6,94 @@
 row = 3
 col = 0
 Loop = True
+Keys = False
 
+#Empty list for inventory
 inventory = []
 
+#This function detects if you are in a room with an item and gives the choice to pick it up
 def Get_Item():
-    global inventory, row, col
-    if row == 0 and col == 1 or row == 2 and col == 2:
+    global inventory, row, col, Keys
+    if row == 0 and col == 1:
+        print(f"\n{items[layout[row][col]]['Item']}")
         choice3 = input("Choice: ")
         if choice3 == "Yes":
             inventory.append("Medkit")
-            print("You picked up the Medkit")
+            print("\nYou picked up the Medkit\n")
+            move()
         elif choice3 == "No":
-            print("You did not pick up the Medkit")
+            print("\nYou did not pick up the Medkit\n")
+            move()
         else:
-            print("I didn't understand that")
+            print("\nI didn't understand that")
             Get_Item()
-        
+    elif row == 2 and col == 2:
+        print(f"\n{items[layout[row][col]]['Item']}")
+        choice4 = input("Choice: ")
+        if choice4 == "Yes":
+            inventory.append("Keys")
+            print("\nYou picked up the keys\n")
+            Keys = True
+            move()
+        elif choice4 == "No":
+            print("\nYou did not pick up the keys\n")
+            move()
+#This function detects if you are on the end tile with the keys, thus winning the game
+def Win():
+    global Loop, Keys
+    if row == 0 and col == 3 and Keys == True:
+        Loop = False
+    elif row == 0 and col == 3 and Keys == False:
+        print("You find an empty escape pod but there's no key for the ignition")
+        move()
     
   
 
 #Lists containing layout of the different rooms in the map
 layout = [['Enemy Room', 'Supply Room', 'Safe Room', 'Escape Pod'],
           ['Safe Room',  'Safe Room',  'Enemy Room', 'Safe Room'],
-          ['Safe Room',  'Safe Room',  'Supply Room',  'Enemy Room'],
+          ['Safe Room',  'Safe Room',  'Janitor Closet',  'Enemy Room'],
           ['Starting Room', 'Safe Room', 'Enemy Room', 'Safe Room'],]
+#Nested dictionaries describing the rooms and their effects
 Tile_Descriptions = {
     "Safe Room" : {
-        "Description" : "Empty room",
-        "Effect" : "None" },
+        "Description" : "You enter an empty room",
+        "Effect" : "You ponder your next actions" },
     "Starting Room" : {
-        "Description" : "Room you begin the game in",
-        "Effect" : "None" },
+        "Description" : "You find yourself on an enemy ship and must escape"},
     "Enemy Room" : {
-        "Description" : "A room with an enemy inside",
+        "Description" : "You enter room with an enemy inside",
         "Effect" : "You lose 2 health" },
     "Escape Pod" : {
-        "Description" : "An escape pod at the end of the ship",
+        "Description" : "You find a lone escape pod at the end of the ship",
         "Effect" : "You win" },
     "Supply Room" : {
-        "Description" : "You find a small storage room with a medkit hanging on the wall",
+        "Description" : "You find a small storage room with a Medkit hanging on the wall",
+        "Effect" : "Do you choose to pick it up?" },
+    "Janitor Closet" : {
+        "Description" : "You find a small janitor's closet with a pair of keys dangling off a key hook",
         "Effect" : "Do you choose to pick it up?",
-        "Item" : "Medkit"
     }
 }
+
+#Nested dictionary of all the items 
+items = {
+    "Safe Room" : {
+        "Item" : "None" },
+    "Starting Room" : {
+        "Item" : "None" },
+    "Enemy Room" : {
+        "Item" : "None" },
+    "Supply Room" : {
+        "Item" : "Medkit" },
+    "Janitor Closet" : {
+        "Item" : "Keys" }
+    } 
+ 
 #This function moves the player around based on their input
 #This function also detects if a player is trying to go out of bounds and prevents them
 def move():
-    global row, col, Loop
+    global row, col, Loop, inventory
     print("Options for action:\nQuit, Move, Inventory\n")    
     choice = input("Action: ")
     if choice == "Quit":
@@ -64,38 +105,52 @@ def move():
         if choice2 == "N":
             if row > 0:
                 row -= 1
+                print(f"\n{Tile_Descriptions[layout[row][col]]['Description']}")
+                print(f"{Tile_Descriptions[layout[row][col]]['Effect']}\n")
             elif row == 0:
-                print("You have hit a wall.\n")
+                print("\nYou have hit a wall.\n")
         elif choice2 == "S":
             if row < 3:
                 row += 1
+                print(f"\n{Tile_Descriptions[layout[row][col]]['Description']}")
+                print(f"{Tile_Descriptions[layout[row][col]]['Effect']}\n")
             elif row == 3:
-                print("You have hit a wall.\n")
+                print("\nYou have hit a wall.\n")
         elif choice2 == "W":
             if col > 0:
                 col -= 1
+                print(f"\n{Tile_Descriptions[layout[row][col]]['Description']}")
+                print(f"{Tile_Descriptions[layout[row][col]]['Effect']}\n")
             elif col == 0:
-                print("You have hit a wall.\n")
+                print("\nYou have hit a wall.\n")
         elif choice2 == "E":
             if col < 3:
                 col += 1
+                print(f"\n{Tile_Descriptions[layout[row][col]]['Description']}")
+                print(f"{Tile_Descriptions[layout[row][col]]['Effect']}\n")
             elif col == 3:
-                 print("You have hit a wall.\n")
+                 print("\nYou have hit a wall.\n")
         elif choice2 == "Quit":
             print("Quitting Game")
             Loop = False
         else:
             print("I didn't understand that\n")
+
+    elif choice == "Inventory":
+        if len(inventory) == 0:
+            print("Your inventory is empty")
+        else:
+            for item in inventory:
+                print(f"-{item}")
     else:
-      print("I didn't understand that\n")
+      print("\nI didn't understand that\n")
 
       
 
-#This will loop the code as long as Loop is true
+#Introduction to situation
+print(f"{Tile_Descriptions[layout[row][col]]['Description']}\n")
+#This will loop the code until they win the game or quit
 while Loop == True:
-    print(f"Location: {layout[row][col]}")
-    print(f"{Tile_Descriptions[layout[row][col]]['Description']}")
-    print(f"{Tile_Descriptions[layout[row][col]]['Effect']}\n")
-    if row == 0 and col == 3:
-        Loop = False
-    move()
+   Get_Item()
+   move()
+   Win()
